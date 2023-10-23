e2e826acbdbbd348f6f40fd6214f1f700644d9b7
WIN11OHS20753\taiyo <taiyou.912@icloud.com>
2023-10-23 17:00:48 +0900

    Inputクラス作成

---
+++ D3D_DEMO_231011/D3D_DEMO_231011/Input.h
@@ -0,0 +1,15 @@
+#pragma once
+#pragma comment (lib, "xinput.lib")
+#include <windows.h>
+#include <xinput.h>
+
+class Input
+{
+};
+
+DWORD XInputGetState(
+    DWORD dwUserIndex,    // コントローラーのインデックス
+    XINPUT_STATE* pState  // 状態を受け取る XINPUT_STATE 構造体へのポインター
+);
+//DIJOYSTATE pad_data;
+
