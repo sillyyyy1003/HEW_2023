0ebf09f01e0b6487003ce12858ad94a9aea9042a
OHS_25020_Kaku <guoning1003@gmail.com>
2023-11-23 19:36:59 +0900

    231123 Kaku：影移動関数作成完了

--- D3D_DEMO_231011/D3D_DEMO_231011/GameObject.cpp
+++ D3D_DEMO_231011/D3D_DEMO_231011/GameObject.cpp
@@ -29,41 +29,61 @@ DirectX::XMFLOAT3 GameObject::GenerateShadowPos(DirectX::XMFLOAT3 lightPos)
 	// 単位ベクトル化する
 	//オブジェクトの位置取得
 	const XMFLOAT3 objPos = m_obj->m_pos; 
-	//ベクトル計算用の型に入れる
+
+	/*
+	////ベクトル計算用の型に入れる
+	//XMVECTOR objVector = XMLoadFloat3(&objPos);
+	//XMVECTOR lightVector = XMLoadFloat3(&lightPos);
+	////光からオブジェクトのベクトルを計算する
+	//XMVECTOR directionVector = XMVectorSubtract(objVector, lightVector);
+	////正規化する
+	//directionVector = XMVector3Normalize(directionVector);
+
+	////ベクトルとx/y軸の内積を計算する
+	//float xProduct = XMVectorGetX(XMVector3Dot(directionVector, XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f))); 
+	//float yProduct = XMVectorGetX(XMVector3Dot(directionVector, XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)));
+	//
+	////ラジアンを計算する
+	//float xangle = asin(xProduct); 
+	//float yangle = acos(yProduct); 
+	//
+	////ラジアンから角度に変換
+	//float xDegrees = DirectX::XMConvertToDegrees(xangle); 
+	//float yDegrees = DirectX::XMConvertToDegrees(yangle); 
+	//
+	////長さを計算する
+	//float zLenth = m_shadow->m_obj->m_pos.z - lightPos.z;
+	//float xLenth = zLenth / tan(xDegrees);
+	//float yLenth = zLenth / tan(yDegrees);
+
+	////角度の中心位置を計算する
+	//float posX = xLenth;
+	//
+	//float posY = yLenth;
+
+
+	//XMFLOAT3 shadowPos = XMFLOAT3(posX, posY, m_shadow->m_obj->m_pos.z);
+	//
+	////できた値を返す
+	//return shadowPos;
+	*/
+
 	XMVECTOR objVector = XMLoadFloat3(&objPos);
 	XMVECTOR lightVector = XMLoadFloat3(&lightPos);
-	//光からオブジェクトのベクトルを計算する
 	XMVECTOR directionVector = XMVectorSubtract(objVector, lightVector);
-	//正規化する
-	directionVector = XMVector3Normalize(directionVector);
 
-	//ベクトルとx/y軸の内積を計算する
-	float xProduct = XMVectorGetX(XMVector3Dot(directionVector, XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f))); 
-	float yProduct = XMVectorGetX(XMVector3Dot(directionVector, XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)));
-	
-	//ラジアンを計算する
-	float xangle = asin(xProduct); 
-	float yangle = acos(yProduct); 
-	
-	//ラジアンから角度に変換
-	float xDegrees = DirectX::XMConvertToDegrees(xangle); 
-	float yDegrees = DirectX::XMConvertToDegrees(yangle); 
-	
-	//長さを計算する
-	float zLenth = m_shadow->m_obj->m_pos.z - lightPos.z;
-	float xLenth = zLenth / tan(xDegrees);
-	float yLenth = zLenth / tan(yDegrees);
+	// 假设墙壁垂直于地面，沿Z轴
+	float wallZ = m_shadow->m_obj->m_pos.z;  // 墙壁的Z坐标
 
-	//角度の中心位置を計算する
-	float posX = xLenth;
-	
-	float posY = yLenth;
+	// 计算交点的Z值等于墙壁的Z值时的t
+	float t = (wallZ - lightPos.z) / XMVectorGetZ(directionVector);
 
+	XMFLOAT3 shadowPosition;
+	shadowPosition.x = lightPos.x + t * XMVectorGetX(directionVector);
+	shadowPosition.y = lightPos.y + t * XMVectorGetY(directionVector);
+	shadowPosition.z = wallZ;  // 墙面的Z值
 
-	XMFLOAT3 shadowPos = XMFLOAT3(posX, posY, m_shadow->m_obj->m_pos.z);
-	
-	//できた値を返す
-	return shadowPos;
+	return shadowPosition;
 }
 
 void GameObject::Update(DirectX::XMFLOAT3 lightPos)
